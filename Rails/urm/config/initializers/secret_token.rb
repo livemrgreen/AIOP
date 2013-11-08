18e4c0a44f8551dfb930249e41c5b32b20b82745
Yannick Paz <livemrgreen@gmail.com>
2013-11-08 16:07:11 +0100

    Added urm app in Rails folder

---
+++ Rails/urm/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Urm::Application.config.secret_key_base = 'f1f64f4a6bccae82d65e723a83e84718370506f0c3f7d7a4cde7a88ca0f869300e9168ce68d4b5f2f0826d0d7bbacd693e1db0c7016728353a64bea1e07ec0b9'
