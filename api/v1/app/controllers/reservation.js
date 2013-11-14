edc901c6a2ff7d192e9d72dcbcbe1269bae29fd5
LitzOuille <litzouille@live.fr>
2013-11-14 11:07:19 +0100

    handleReservation

--- api/v1/app/controllers/reservation.js
+++ api/v1/app/controllers/reservation.js
@@ -1,4 +1,5 @@
 var orm = require("../../config/models");
+var async = require("async");
 
 /*
  * GET /reservations/
@@ -7,7 +8,9 @@ module.exports.list = function (req, res, next) {
     var reservation = orm.model("reservation");
 
     reservation.findAll({"where": {"deleted_at": null}}).success(function (reservations) {
-        res.send(200, {"reservations": reservations});
+        async.map(reservations, handleReservation, function (error, results) {
+            res.send(200, {"reservations": results});
+        });
     });
 
     return next();
@@ -118,4 +121,89 @@ module.exports.delete = function (req, res, next) {
     });
 
     return next();
+};
+
+/*
+ * HELPERS
+ */
+var handleReservation = function (reservation, done) {
+    var tmp = reservation.values;
+    delete tmp.time_slot_id;
+    delete tmp.room_id;
+    delete tmp.teaching_id;
+    delete tmp.reservation_request_id;
+
+    async.parallel(
+        // first arg = tasks to do
+        {
+            // try to reach the administrator
+            "time_slot": function (done) {
+                reservation.getSlot().success(function (time_slot) {
+                    if (time_slot) {
+                        var t = time_slot.values;
+                        done(null, t);
+                    }
+                    else {
+                        done(null);
+                    }
+                });
+            },
+
+            "room": function (done) {
+                reservation.getRoom().success(function (room) {
+                    if (room) {
+                        var r = room.values;
+                        room.getBuilding().success(function (building) {
+                            if (building) {
+                                r.building = building.values;
+                                done(null, r);
+                            }
+                            else {
+                                done(null);
+                            }
+                        });
+                    }
+                    else {
+                        done(null);
+                    }
+                });
+            },
+
+            "teaching": function (done) {
+                reservation.getTeaching().success(function (teaching) {
+                    if (teaching) {
+                        var t = teaching.values;
+                        done(null, t);
+                    }
+                    else {
+                        done(null);
+                    }
+                });
+            },
+
+            // try to reach the manager
+            "reservation_request": function (done) {
+                teacher.getRequest().success(function (reservation_request) {
+                    if (reservation_request) {
+                        var r = reservation_request.values;
+                        done(null, r);
+                    }
+                    else {
+                        done(null);
+                    }
+                });
+            }
+        },
+
+        // second arg = final func
+        function (err, results) {
+            // when done, we can use the object-key
+            // to get the object-value
+            tmp.time_slot = results.time_slot;
+            tmp.room = results.room;
+            tmp.teaching = results.teaching;
+            tmp.reservation_request = results.reservation_request;
+            done(null, tmp);
+        }
+    );
 };
