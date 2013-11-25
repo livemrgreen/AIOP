19a82ceb32113a0d664075846b7b55ac4bb2c50d
Clément Fiorio <clement.fiorio@gmail.com>
2013-11-25 18:58:14 +0100

    reorg code home-ctrl

--- src/app/modules/home/home-ctrl.js
+++ src/app/modules/home/home-ctrl.js
@@ -8,13 +8,10 @@ define([
     'use strict';
 
     app.register.controller('HomeController', function($rootScope, $scope, $http, $filter, $location, LocalStorageService, UserService, UrlService) {
-
         var date = new Date();
         var d = date.getDate();
         var m = date.getMonth();
         var y = date.getFullYear();
-
-
         var apiUrl = UrlService.urlNode;
         $scope.calendarName = '';
 
@@ -26,90 +23,93 @@ define([
         /**
          * Get all groups
          */
-        $http({method: 'Get', url: apiUrl+'/groups', headers: {'Authorization': "Bearer " + UserService.getAccessToken() + ""}}).
-            success(function (data) {
-                console.log(data);
-                $scope.groups = data.groups;
-            }).
-            error(function (data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                //todo:trtaiter l'erreur
-                console.log(status);
-                console.log(data);
-            });
+        $http({
+            method: 'Get',
+            url: apiUrl + '/groups',
+            headers: {
+                'Authorization': "Bearer " + UserService.getAccessToken() + ""
+            }})
+                .success(function(data) {
+            $scope.groups = data.groups;
+        })
+                .error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            //todo:trtaiter l'erreur
+            console.log(status);
+            console.log(data);
+        });
 
         $scope.teachers = [];
         /**
          * Get all groups
          */
-        $http({method: 'Get', url: apiUrl+'/teachers', headers: {'Authorization': "Bearer " + UserService.getAccessToken() + ""}}).
-            success(function (data) {
-                $scope.teachers = data.teachers;
-            }).
-            error(function (data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                //todo:trtaiter l'erreur
-                console.log(status);
-                console.log(data);
-            });
+        $http({method: 'Get', url: apiUrl + '/teachers', headers: {'Authorization': "Bearer " + UserService.getAccessToken() + ""}}).
+                success(function(data) {
+            $scope.teachers = data.teachers;
+        })
+                .error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            //todo:trtaiter l'erreur
+            console.log(status);
+            console.log(data);
+        });
 
-        $http({method: 'Get', url: apiUrl+'/teachers/'+UserService.getUser().teacher.id+'/reservations', headers: {'Authorization': "Bearer " + UserService.getAccessToken() + ""}}).
-            success(function(data) {
-                console.log('success');
-                console.log(data);
+        $http({
+            method: 'Get',
+            url: apiUrl + '/teachers/' + UserService.getUser().teacher.id + '/reservations',
+            headers: {
+                'Authorization': "Bearer " + UserService.getAccessToken() + ""
+            }})
+                .success(function(data) {
+            $scope.calendarName = UserService.getUser().teacher.first_name + ' ' + UserService.getUser().teacher.last_name;
 
-                $scope.calendarName = UserService.getUser().teacher.first_name+' '+UserService.getUser().teacher.last_name;
+            angular.forEach(data.reservations, function(value, key) {
+                var min = value.time_slot.start.split(':');
+                min = min[1];
+                var start = new Date(value.date);
+                start.setHours(parseInt(value.time_slot.start));
+                start.setMinutes(parseInt(min));
+                var end = new Date(value.date);
+                min = value.time_slot.end.split(':');
+                min = min [1];
 
+                end.setHours(parseInt(value.time_slot.end));
+                end.setMinutes(parseInt(min));
 
-                angular.forEach(data.reservations, function(value, key){
-
-                    var min = value.time_slot.start.split(':');
-                    min = min[1];
-
-                    var start = new Date(value.date);
-                    start.setHours(parseInt(value.time_slot.start));
-                    start.setMinutes(parseInt(min));
-
-                    var end = new Date(value.date);
-                    min = value.time_slot.end.split(':');
-                    min = min [1];
-
-                    end.setHours(parseInt(value.time_slot.end));
-                    end.setMinutes(parseInt(min));
-
-                   $scope.events.push(
-                       {
-                           title: value.teaching.lesson.subject.label + '\n' +value.teaching.group.label+' '+ value.room.label, start: start, end: end, allDay: false
-                       }
-                   );
+                $scope.events.push({
+                    title: value.teaching.lesson.subject.label + '\n' + value.teaching.group.label + ' ' + value.room.label,
+                    start: start,
+                    end: end,
+                    allDay: false
                 });
-            })
-                    .error(function(data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                console.log(status);
-                console.log(data);
             });
+        })
+                .error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log(status);
+            console.log(data);
+        });
 
         $scope.classReservations = 'collapse';
         $scope.admin = UserService.isAdmin();
         $scope.open = function(div) {
-            if (div === 'admin'){
+            if (div === 'admin') {
                 var $icon = $('#accordion-toggle-admin').children('span').children('i');
                 //open or close submenu of reservation
                 if ($scope.classReservationsAdmin === 'collapse') {
                     //change arrow value of Reservations
                     $icon.removeClass('fa fa-angle-left').addClass('fa fa-angle-down');
-                    $scope.classReservationsAdmin= '';
+                    $scope.classReservationsAdmin = '';
                 }
                 else {
                     $icon.removeClass('fa fa-angle-down').addClass('fa fa-angle-left');
                     $scope.classReservationsAdmin = 'collapse';
                 }
             }
-            else{
+            else {
                 var $icon = $('#accordion-toggle').children('span').children('i');
                 //open or close submenu of reservation
                 if ($scope.classReservations === 'collapse') {
@@ -209,78 +209,74 @@ define([
         $scope.eventSources = [$scope.events];
 
 
-        $scope.getGroupCalendar= function(group){
-            $http({method: 'Get', url: apiUrl+'/groups/'+group.id+'/reservations', headers: {'Authorization': "Bearer " + UserService.getAccessToken() + ""}}).
-                success(function (data) {
-                    console.log(data);
-                    $scope.calendarName = $scope.group.label;
-                    angular.forEach(data.reservations, function(value, key){
+        $scope.getGroupCalendar = function(group) {
+            $http({
+                method: 'Get',
+                url: apiUrl + '/groups/' + group.id + '/reservations',
+                headers: {
+                    'Authorization': "Bearer " + UserService.getAccessToken() + ""
+                }})
+                    .success(function(data) {
+                $scope.calendarName = $scope.group.label;
+                angular.forEach(data.reservations, function(value, key) {
+                    var min = value.time_slot.start.split(':');
+                    min = min[1];
+                    var start = new Date(value.date);
+                    start.setHours(parseInt(value.time_slot.start));
+                    start.setMinutes(parseInt(min));
+                    var end = new Date(value.date);
+                    min = value.time_slot.end.split(':');
+                    min = min [1];
+                    end.setHours(parseInt(value.time_slot.end));
+                    end.setMinutes(parseInt(min));
 
-                        var min = value.time_slot.start.split(':');
-                        min = min[1];
-
-                        var start = new Date(value.date);
-                        start.setHours(parseInt(value.time_slot.start));
-                        start.setMinutes(parseInt(min));
-
-                        var end = new Date(value.date);
-                        min = value.time_slot.end.split(':');
-                        min = min [1];
-
-                        end.setHours(parseInt(value.time_slot.end));
-                        end.setMinutes(parseInt(min));
-
-                        $scope.events.push(
-                            {
-                                title: value.teaching.lesson.subject.label + '\n' +value.teaching.group.label+' '+ value.room.label, start: start, end: end, allDay: false
-                            }
-                        );
+                    $scope.events.push({
+                        title: value.teaching.lesson.subject.label + '\n' + value.teaching.group.label + ' ' + value.room.label, start: start, end: end, allDay: false
                     });
-                }).
-                error(function (data, status, headers, config) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                    //todo:trtaiter l'erreur
-                    console.log(status);
-                    console.log(data);
                 });
+            })
+                    .error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                //todo:trtaiter l'erreur
+                console.log(status);
+                console.log(data);
+            });
         };
 
-        $scope.getTeacherCalendar= function(teacher){
-            $http({method: 'Get', url: apiUrl+'/teachers/'+teacher.id+'/reservations', headers: {'Authorization': "Bearer " + UserService.getAccessToken() + ""}}).
-                success(function (data) {
-                    console.log(data);
-                    $scope.calendarName = $scope.teacher.first_name+$scope.teacher.last_name;
-                    angular.forEach(data.reservations, function(value, key){
+        $scope.getTeacherCalendar = function(teacher) {
+            $http({
+                method: 'Get',
+                url: apiUrl + '/teachers/' + teacher.id + '/reservations',
+                headers: {
+                    'Authorization': "Bearer " + UserService.getAccessToken() + ""
+                }})
+                    .success(function(data) {
+                $scope.calendarName = $scope.teacher.first_name + $scope.teacher.last_name;
+                angular.forEach(data.reservations, function(value, key) {
+                    var min = value.time_slot.start.split(':');
+                    min = min[1];
+                    var start = new Date(value.date);
+                    start.setHours(parseInt(value.time_slot.start));
+                    start.setMinutes(parseInt(min));
+                    var end = new Date(value.date);
+                    min = value.time_slot.end.split(':');
+                    min = min [1];
+                    end.setHours(parseInt(value.time_slot.end));
+                    end.setMinutes(parseInt(min));
 
-                        var min = value.time_slot.start.split(':');
-                        min = min[1];
-
-                        var start = new Date(value.date);
-                        start.setHours(parseInt(value.time_slot.start));
-                        start.setMinutes(parseInt(min));
-
-                        var end = new Date(value.date);
-                        min = value.time_slot.end.split(':');
-                        min = min [1];
-
-                        end.setHours(parseInt(value.time_slot.end));
-                        end.setMinutes(parseInt(min));
-
-                        $scope.events.push(
-                            {
-                                title: value.teaching.lesson.subject.label + '\n' +value.teaching.group.label+' '+ value.room.label, start: start, end: end, allDay: false
-                            }
-                        );
+                    $scope.events.push({
+                        title: value.teaching.lesson.subject.label + '\n' + value.teaching.group.label + ' ' + value.room.label, start: start, end: end, allDay: false
                     });
-                })
-                        .error(function (data, status, headers, config) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                    //todo:trtaiter l'erreur
-                    console.log(status);
-                    console.log(data);
                 });
+            })
+                    .error(function(data, status, headers, config) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                //todo:trtaiter l'erreur
+                console.log(status);
+                console.log(data);
+            });
         };
     });
 });
